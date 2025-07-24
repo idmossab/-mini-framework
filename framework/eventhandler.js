c6e05ea688860caa77e55dd6b40a98d5adb714d9
ifares <hmsteven37@gmail.com>
2025-07-24 16:03:11 +0100

    event

--- framework/eventhandler.js
+++ framework/eventhandler.js
@@ -3,19 +3,29 @@ class EventRegistry {
     this.handlers = {
       click: {},
       keydown: {},
+      keyup: {},     
+      dblclick: {},   
       scroll: {}
     };
   }
 
   register(type, id, fn) {
+    if (!this.handlers[type]) {
+      this.handlers[type] = {};
+    }
     this.handlers[type][id] = fn;
   }
 
   dispatch(type, event) {
     let target = event.target;
+
+    if (target.nodeType === Node.TEXT_NODE) {
+      target = target.parentElement;
+    }
+
     while (target && target !== document) {
       const handlerId = target.getAttribute(`data-on${type}`);
-      if (handlerId && this.handlers[type][handlerId]) {
+      if (handlerId && this.handlers[type] && this.handlers[type][handlerId]) {
         this.handlers[type][handlerId](event);
         break;
       }
